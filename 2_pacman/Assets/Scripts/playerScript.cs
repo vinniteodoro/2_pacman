88878d5f0e7b0218cacce9ea71921d668b875549
Vinicius Teodoro <vinitbasilio@hotmail.com>
2022-01-26 01:00:25 -0300

    Creation of movement and player script's
    BUG: The player is not moving yet, but technically the code is ok, have to revise it.

---
+++ 2_pacman/Assets/Scripts/playerScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public movementScript movementScript { get; private set; }
+
+    private void Awake()
+    {
+        movementScript = GetComponent<movementScript>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow)) movementScript.SetObjectDirection(Vector2.up);
+        if(Input.GetKeyDown(KeyCode.DownArrow)) movementScript.SetObjectDirection(Vector2.down);
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) movementScript.SetObjectDirection(Vector2.left);
+        if(Input.GetKeyDown(KeyCode.RightArrow)) movementScript.SetObjectDirection(Vector2.right);
+    }
+}
