e1b0bf168973895668388c6d4d6fcfd495e17b36
Vinicius Teodoro <vinitbasilio@hotmail.com>
2022-01-29 19:34:09 -0300

    Choose how many ghosts to play with

--- 2_pacman/Assets/Scripts/gameManagerScript.cs
+++ 2_pacman/Assets/Scripts/gameManagerScript.cs
@@ -9,6 +9,11 @@ public class gameManagerScript : MonoBehaviour
    [SerializeField] GameObject mainMenuObject;
    [SerializeField] GameObject winMenuObject;
    [SerializeField] coinScript coinScript;
+   [SerializeField] GameObject OrangeGhost;
+   [SerializeField] GameObject BlueGhost;
+   [SerializeField] GameObject PinkGhost;
+   [SerializeField] GameObject RedGhost;
+   private int ghostsQuantity;
 
    private void Update()
    {
@@ -31,7 +36,7 @@ public class gameManagerScript : MonoBehaviour
          coinsObject.SetActive(false);
          wallsObject.SetActive(false);
          winMenuObject.SetActive(true);
-
+         DeSpawnGhosts();
          DoNotTriggerWinConditionsAgain();
       }
    }
@@ -44,5 +49,85 @@ public class gameManagerScript : MonoBehaviour
    public void PlayAgain()
    {
       SceneManager.LoadScene("Main");
+   }
+
+   public void spawnOneGhost()
+   {
+      ghostsQuantity = 1;
+      SpawnGhosts();
+   }
+
+   public void spawnTwoGhosts()
+   {
+      ghostsQuantity = 2;
+      SpawnGhosts();
+   }
+
+   public void spawnThreeGhosts()
+   {
+      ghostsQuantity = 3;
+      SpawnGhosts();
+   }
+
+   public void spawnFourGhosts()
+   {
+      ghostsQuantity = 4;
+      SpawnGhosts();
+   }
+
+   public void DeSpawnGhosts()
+   {
+      if(ghostsQuantity == 1) 
+      {
+         RedGhost.SetActive(false);
+      }
+      else if(ghostsQuantity == 2) 
+      {
+         RedGhost.SetActive(false);
+         OrangeGhost.SetActive(false);
+      }
+      else if(ghostsQuantity == 3) 
+      {
+         RedGhost.SetActive(false);
+         OrangeGhost.SetActive(false);
+         BlueGhost.SetActive(false);
+      }
+      else
+      {
+         RedGhost.SetActive(false);
+         OrangeGhost.SetActive(false);
+         BlueGhost.SetActive(false);
+         PinkGhost.SetActive(false);
+      }
+   }
+
+   public void SpawnGhosts()
+   {
+      if(ghostsQuantity == 1) 
+      {
+         RedGhost.SetActive(true);
+         PlayGame();
+      }
+      else if(ghostsQuantity == 2) 
+      {
+         RedGhost.SetActive(true);
+         OrangeGhost.SetActive(true);
+         PlayGame();
+      }
+      else if(ghostsQuantity == 3) 
+      {
+         RedGhost.SetActive(true);
+         OrangeGhost.SetActive(true);
+         BlueGhost.SetActive(true);
+         PlayGame();
+      }
+      else
+      {
+         RedGhost.SetActive(true);
+         OrangeGhost.SetActive(true);
+         BlueGhost.SetActive(true);
+         PinkGhost.SetActive(true);
+         PlayGame();
+      }
    }
 }
