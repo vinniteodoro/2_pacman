9537d15358436ebaa0f45032c61c2d42c6b42d2e
Vinicius Teodoro <vinitbasilio@hotmail.com>
2022-01-30 18:56:31 -0300

    Fixed all movement bugs of the ghosts

--- 2_pacman/Assets/Scripts/ghostsMovementScript.cs
+++ 2_pacman/Assets/Scripts/ghostsMovementScript.cs
@@ -114,7 +114,7 @@ public class ghostsMovementScript : MonoBehaviour
 
     private void ChangeDirectionWhenAvailable()
     {
-        ghostRaycast = Physics2D.BoxCast(objectRigidBody.position, new Vector2(.95f, .95f), 0, nextChosenDirection, 1, obstaclesLayerMask);
+        ghostRaycast = Physics2D.BoxCast(objectRigidBody.position, new Vector2(.75f, .75f), 0, nextChosenDirection, 1, obstaclesLayerMask);
         if(ghostRaycast.collider == null) 
         {
             Debug.DrawRay(objectRigidBody.position, nextChosenDirection, Color.green); 
