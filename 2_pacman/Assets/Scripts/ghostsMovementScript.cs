9ec0b4b86843113db87eb83c9b8b727fee1a1977
Vinicius Teodoro <vinitbasilio@hotmail.com>
2022-01-30 16:28:42 -0300

    Created fundamentals for ghosts' movement
    Create raycasts needed to decide which path the ghost can go based on the intersection tile it collided

--- 2_pacman/Assets/Scripts/ghostsMovementScript.cs
+++ 2_pacman/Assets/Scripts/ghostsMovementScript.cs
@@ -6,6 +6,12 @@ public class ghostsMovementScript : MonoBehaviour
     private Rigidbody2D objectRigidBody;
     private float objectSpeed = 7f;
     private Vector2 objectDirection;
+    private Vector3Int tileToRaycastFrom, objectGridPosition;
+    private ContactPoint2D[] colliderContactPoint = new ContactPoint2D[1];
+    [SerializeField] Grid grid;
+    [SerializeField] LayerMask obstaclesLayerMask;
+    private RaycastHit2D upRaycast, downRaycast, leftRaycast, rightRaycast;
+    private Vector2 rayCastOriginPosition;
 
     private void Awake()
     {
@@ -21,6 +27,7 @@ public class ghostsMovementScript : MonoBehaviour
     {
         MoveObject();
         VerifyMapBoundaries();
+        DrawRaycasts();
     }
 
     private void RandomizeStartingGhostsDirection()
@@ -36,8 +43,60 @@ public class ghostsMovementScript : MonoBehaviour
     {
         if(otherCollider.gameObject.tag == "Intersection")
         {
-            
+            PrepareTileToRaycastFrom();
+            GenerateRaycastsInAllDirections();
+            DrawRaycasts();
         }
+    }
+
+    private void PrepareTileToRaycastFrom()
+    {
+        objectGridPosition = grid.WorldToCell(objectRigidBody.position);
+
+        if(objectDirection.x == -1) tileToRaycastFrom = objectGridPosition + new Vector3Int(-1, 0, 0);
+        if(objectDirection.x == 1) tileToRaycastFrom = objectGridPosition + new Vector3Int(1, 0, 0);
+        if(objectDirection.y == -1) tileToRaycastFrom = objectGridPosition + new Vector3Int(0, -1, 0);
+        if(objectDirection.y == 1) tileToRaycastFrom = objectGridPosition + new Vector3Int(0, 1, 0);
+
+        rayCastOriginPosition = new Vector2(tileToRaycastFrom.x + .5f, tileToRaycastFrom.y+.5f);
+    }
+
+    private void GenerateRaycastsInAllDirections()
+    {
+        upRaycast = Physics2D.Raycast(rayCastOriginPosition, Vector2.up, 1, obstaclesLayerMask);
+        downRaycast = Physics2D.Raycast(rayCastOriginPosition, Vector2.down, 1, obstaclesLayerMask);
+        leftRaycast = Physics2D.Raycast(rayCastOriginPosition, Vector2.left, 1, obstaclesLayerMask);
+        rightRaycast = Physics2D.Raycast(rayCastOriginPosition, Vector2.right, 1, obstaclesLayerMask);
+    }
+
+    private void DrawRaycasts()
+    {
+        if(upRaycast.collider == null) Debug.DrawRay(rayCastOriginPosition, Vector2.up, Color.green);
+        else Debug.DrawRay(rayCastOriginPosition, Vector2.up, Color.red);
+        
+        if(downRaycast.collider == null) Debug.DrawRay(rayCastOriginPosition, Vector2.down, Color.green);
+        else Debug.DrawRay(rayCastOriginPosition, Vector2.down, Color.red);
+
+        if(leftRaycast.collider == null) Debug.DrawRay(rayCastOriginPosition, Vector2.left, Color.green);
+        else Debug.DrawRay(rayCastOriginPosition, Vector2.left, Color.red);
+
+        if(rightRaycast.collider == null) Debug.DrawRay(rayCastOriginPosition, Vector2.right, Color.green);
+        else Debug.DrawRay(rayCastOriginPosition, Vector2.right, Color.red);
+    }
+
+    private void SaveRaycastsThatDidntHitAnything()
+    {
+        
+    }
+
+    private void RandomizeWhichPathToGo()
+    {
+        
+    }
+
+    private void WhenToChangeDirection()
+    {
+
     }
 
     private void VerifyMapBoundaries()
